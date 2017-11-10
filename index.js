5a0b4035b47fe2f9558afa034493ee724dc93c7f
nao akakura <nao.akakura@valtes-mt.co.jp>
2017-11-10 21:53:36 +0900

    temp

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 window.addEventListener('scroll', function(){
-  document.getElementById('fixheader').setAttribute('data-fixed', window.pageYOffset > 50 ? '1' : '0');
+  //document.getElementById('fixheader').setAttribute('data-fixed', window.pageYOffset > 50 ? '1' : '0');
+  document.getElementById('fixheader').classList.toggle("fixed");
 }, false);
